4127042030b80c283431cf72ace98530d46beb2d
lenin <lenin12906@gmail.com>
2026-05-16 11:25:00 +0530

    doubly linked list

--- LL.cpp
+++ LL.cpp
@@ -6,15 +6,23 @@ class Node {
     public: 
     int data;
     Node* next;
+    Node* back;
     //constructor for (data, next)
     Node(int data1, Node* next1){
         data = data1;
         next = next1;
+        back = nullptr;
     }
     //constructor for (data)
     Node(int data1){
         data = data1;
         next = nullptr;
+        back = nullptr;
+    }
+    Node(int data1, Node* next1, Node* back1){
+        data = data1;
+        next = next1;
+        back = back1;
     }
 
 };
@@ -36,16 +44,28 @@ int lengthofLL(Node* head){
         temp = temp->next;
     }
     return count;
+};
+Node* convertarr2DLL(vector<int> &arr){
+        Node* head = new Node(arr[0]);
+        Node* prev = head;
+        for(int i=1; i<arr.size(); i++){
+            Node* temp = new Node(arr[i], nullptr, prev);
+            prev-> next = temp;
+            prev = prev->next;
+        }
+    return head;
+    
+
 }
+void print(Node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head = head-> next;
+    }
+};
 int main(){
     vector<int> arr = {22,5,8,7};
-    Node* head = convertArr2LL(arr);
-    Node* temp = head;
-    while(temp){
-        cout<< temp->data <<" ";
-        temp = temp -> next;
-
-    }
-    cout<<"\n";
-    cout<<lengthofLL(head);
-}
+    Node* head = convertarr2DLL(arr);
+    print(head);
+    return 0;
+};
